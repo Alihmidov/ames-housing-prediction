c5f3b46f2378254ac57c756f7b2c2579b2da9877
Hamidov Ali <hemidoveli893@gmail.com>
2026-04-05 21:50:28 +0400

    feat: add database connection logic and load_dotenv for production

--- app/main.py
+++ app/main.py
@@ -3,7 +3,11 @@ import joblib
 import pandas as pd
 import numpy as np
 from fastapi import FastAPI
+from sqlalchemy import create_engine, text
+from dotenv import load_dotenv
 from .schemas import HouseInput 
+
+load_dotenv()
 
 app = FastAPI(title="AMES HOUSING Prediction API")
 
@@ -16,20 +20,43 @@ FEATURES_PATH = os.path.join(MODEL_DIR, "important_features.joblib")
 model = joblib.load(MODEL_PATH)
 features = joblib.load(FEATURES_PATH)
 
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+if not DATABASE_URL:
+    DB_USER = os.getenv("DB_USER")
+    DB_PASSWORD = os.getenv("DB_PASSWORD")
+    DB_HOST = os.getenv("DB_HOST")
+    DB_PORT = os.getenv("DB_PORT")
+    DB_NAME = os.getenv("DB_NAME")
+    DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
+# SQLAlchemy engine yaradırıq
+engine = create_engine(DATABASE_URL)
+
 @app.get("/")
 def home():
-    return {"message": "AMES HOUSING API is running!"}
+    try:
+        with engine.connect() as conn:
+            conn.execute(text("SELECT 1"))
+        db_status = "Connected"
+    except Exception as e:
+        db_status = f"Disconnected: {str(e)}"
+        
+    return {
+        "message": "AMES HOUSING API is running!",
+        "database_status": db_status
+    }
 
 @app.post("/predict")
 def predict(data: HouseInput):
-    input_df = pd.DataFrame([data.model_dump()])
+    input_dict = data.model_dump()
+    input_df = pd.DataFrame([input_dict])
     
     input_df = input_df[features]
     
     log_prediction = model.predict(input_df)[0]
-    
     real_price = np.exp(log_prediction)
-    
+        
     return {
         "estimated_price": round(float(real_price), 2),
         "currency": "USD"
